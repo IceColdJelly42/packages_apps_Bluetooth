77d3bff4fb259735553681a87a727ba5748d6e97
LorD ClockaN <davor@losinj.com>
2013-02-03 23:05:49 +0100

    Revert "Always follow the user's preferred contact display name format."
    This reverts commit 05a30b024396a0e8f8db446d839b30e584df6037.

--- src/com/android/bluetooth/pbap/BluetoothPbapVcardManager.java
+++ src/com/android/bluetooth/pbap/BluetoothPbapVcardManager.java
@@ -44,8 +44,6 @@ import android.provider.ContactsContract.Contacts;
 import android.provider.ContactsContract.Data;
 import android.provider.ContactsContract.CommonDataKinds.Phone;
 import android.provider.ContactsContract.PhoneLookup;
-import android.provider.ContactsContract.Preferences;
-import android.provider.Settings;
 import android.telephony.PhoneNumberUtils;
 import android.text.TextUtils;
 import android.util.Log;
@@ -89,13 +87,12 @@ public class BluetoothPbapVcardManager {
 
     static final String[] CONTACTS_PROJECTION = new String[] {
             Contacts._ID, // 0
-            Contacts.DISPLAY_NAME_PRIMARY, // 1
-            Contacts.DISPLAY_NAME_ALTERNATIVE // 2
+            Contacts.DISPLAY_NAME, // 1
     };
 
     static final int CONTACTS_ID_COLUMN_INDEX = 0;
-    static final int CONTACTS_PRIM_NAME_COLUMN_INDEX = 1;
-    static final int CONTACTS_ALT_NAME_COLUMN_INDEX = 2;
+
+    static final int CONTACTS_NAME_COLUMN_INDEX = 1;
 
     // call histories use dynamic handles, and handles should order by date; the
     // most recently one should be the first handle. In table "calls", _id and
@@ -237,24 +234,6 @@ public class BluetoothPbapVcardManager {
         return list;
     }
 
-    private int getDisplayNameColumnIndex() {
-        int order = Settings.System.getInt(mResolver,
-                Preferences.DISPLAY_ORDER, Preferences.DISPLAY_ORDER_PRIMARY);
-
-        return order == Preferences.DISPLAY_ORDER_ALTERNATIVE
-                ? CONTACTS_ALT_NAME_COLUMN_INDEX
-                : CONTACTS_PRIM_NAME_COLUMN_INDEX;
-    }
-
-    private String getDisplayNameColumn() {
-        int order = Settings.System.getInt(mResolver,
-                Preferences.DISPLAY_ORDER, Preferences.DISPLAY_ORDER_PRIMARY);
-
-        return order == Preferences.DISPLAY_ORDER_ALTERNATIVE
-                ? Contacts.DISPLAY_NAME_ALTERNATIVE
-                : Contacts.DISPLAY_NAME_PRIMARY;
-    }
-
     public final ArrayList<String> getPhonebookNameList(final int orderByWhat) {
         ArrayList<String> nameList = new ArrayList<String>();
         //Owner vCard enhancement. Use "ME" profile if configured
@@ -278,13 +257,12 @@ public class BluetoothPbapVcardManager {
             } else if (orderByWhat == BluetoothPbapObexServer.ORDER_BY_ALPHABETICAL) {
                 if (V) Log.v(TAG, "getPhonebookNameList, order by alpha");
                 contactCursor = mResolver.query(myUri, CONTACTS_PROJECTION, CLAUSE_ONLY_VISIBLE,
-                        null, getDisplayNameColumn());
+                        null, Contacts.DISPLAY_NAME);
             }
             if (contactCursor != null) {
-                int nameIndex = getDisplayNameColumnIndex();
                 for (contactCursor.moveToFirst(); !contactCursor.isAfterLast(); contactCursor
                         .moveToNext()) {
-                    String name = contactCursor.getString(nameIndex);
+                    String name = contactCursor.getString(CONTACTS_NAME_COLUMN_INDEX);
                     if (TextUtils.isEmpty(name)) {
                         name = mContext.getString(android.R.string.unknownName);
                     }
@@ -317,10 +295,9 @@ public class BluetoothPbapVcardManager {
                         null, Contacts._ID);
 
             if (contactCursor != null) {
-                int nameIndex = getDisplayNameColumnIndex();
                 for (contactCursor.moveToFirst(); !contactCursor.isAfterLast(); contactCursor
                         .moveToNext()) {
-                    String name = contactCursor.getString(nameIndex);
+                    String name = contactCursor.getString(CONTACTS_NAME_COLUMN_INDEX);
                     long id = contactCursor.getLong(CONTACTS_ID_COLUMN_INDEX);
                     if (TextUtils.isEmpty(name)) {
                         name = mContext.getString(android.R.string.unknownName);
@@ -470,7 +447,7 @@ public class BluetoothPbapVcardManager {
         } else if (orderByWhat == BluetoothPbapObexServer.ORDER_BY_ALPHABETICAL) {
             try {
                 contactCursor = mResolver.query(myUri, CONTACTS_PROJECTION, CLAUSE_ONLY_VISIBLE,
-                        null, getDisplayNameColumn());
+                        null, Contacts.DISPLAY_NAME);
                 if (contactCursor != null) {
                     contactCursor.moveToPosition(offset - 1);
                     contactId = contactCursor.getLong(CONTACTS_ID_COLUMN_INDEX);
@@ -513,12 +490,6 @@ public class BluetoothPbapVcardManager {
                     vcardType |= VCardConfig.FLAG_REFRAIN_IMAGE_EXPORT;
                 }
 
-                int order = Settings.System.getInt(mResolver,
-                        Preferences.DISPLAY_ORDER, Preferences.DISPLAY_ORDER_PRIMARY);
-                if (order == Preferences.DISPLAY_ORDER_ALTERNATIVE) {
-                    vcardType |= VCardConfig.FLAG_USE_ALTERNATIVE_NAME_ORDERING;
-                }
-
                 //Enhancement: customize Vcard based on preferences/settings and input from caller
                 composer = BluetoothPbapUtils.createFilteredVCardComposer(mContext, vcardType,null);
                 //End enhancement
@@ -539,11 +510,8 @@ public class BluetoothPbapVcardManager {
                             }
                         });
                 buffer = new HandlerForStringBuffer(op, ownerVCard);
-                if (!composer.init(Contacts.CONTENT_URI, CONTACTS_PROJECTION,
-                            selection, null, Contacts._ID, null)) {
-                    return ResponseCodes.OBEX_HTTP_INTERNAL_ERROR;
-                }
-                if (!buffer.onInit(mContext)) {
+                if (!composer.init(Contacts.CONTENT_URI, selection, null, Contacts._ID) ||
+                        !buffer.onInit(mContext)) {
                     return ResponseCodes.OBEX_HTTP_INTERNAL_ERROR;
                 }
 
